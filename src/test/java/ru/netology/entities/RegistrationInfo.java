bc2fe6700898eddf662a6bbf18723fce6c4ab926
Sergey Scherbakov <sherbakov_s_a@mail.ru>
2022-04-04 21:31:53 +0300

    fxed by comment

--- src/test/java/ru/netology/entities/RegistrationInfo.java
+++ src/test/java/ru/netology/entities/RegistrationInfo.java
@@ -7,9 +7,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class RegistrationInfo {
     private final String city;
-    private final String dateMeeting;
-    private final String dateMeetingOut;
-    private final String dateMeetingInvalid;
     private final String name;
     private final String phoneNumber;
 }
